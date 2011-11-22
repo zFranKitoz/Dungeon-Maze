58e6085df5492ddd21d603adb600757657cc3048
Gianfranco Soto <FK_FranKo@hotmail.com>
2011-11-22 13:29:40 -0500

    corner

---
+++ Scripts/messages.js
@@ -0,0 +1,13 @@
+/*messages on down log*/
+
+
+function writeOnLOg(log,message){//(type,log,message){
+
+		//if(type == 'new')
+			log.text(''+message);
+		//log.text(''+message);
+			
+}	
+function clearLastLog(log){
+		
+}
